db8a2658cb87f4d418307a93c6b68a9a9ef34c3f
ThatLivelyLlama <thatlivelyllama@gmail.com>
2021-04-02 08:32:07 -0600

    Developed player and now working on the Deck class to populate the deck of each player.

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-let suitArray = ["hearts", "spades", "clubs", "diamonds"]
-let rankArray = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
-let scoreArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// Deck parts
+let suitArray = ["hearts", "spades", "clubs", "diamonds"];
+let rankArray = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
+let scoreArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+let fullDeck = [];
+
 
 class Card {
     constructor(suit, rank, score) {
         this.suit =suit;
         this.rank = rank;
         this.score = score;
+    }
+}
+
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.deck = []
+    }
+}
+
+class Deck {
+    constructor(cards) {
+        this.length = cards.length;
+        this.cards = cards;
     }
 }
